5d70ba0e7a6f39fadd937408a8e6a40f5ead1b80
Tristan Kay <tridkay90@gmail.com>
2019-05-30 15:52:58 +1000

    first leaflet map complete

--- firstLeaflet.js
+++ firstLeaflet.js
@@ -1,9 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+// knitializing the map and set its view to a chosen geographical 
+// coordinates and zoom level.
+// By default all mouse and touch interactions are enabled when your mouse goes 
+// over the map.
